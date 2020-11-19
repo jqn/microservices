2d60bc1b27726d19b607bbcff89953ca9b9254d3
jqn <jqngl@icloud.com>
2020-11-19 14:24:42 +0100

    Refactored app to use local instance config in local environments and config.py in dev and prod servers

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
 # local imports
 from config import app_config
 
@@ -17,9 +19,16 @@ login_manager = LoginManager()
 
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
 
     Bootstrap(app)
     db.init_app(app)
