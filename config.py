02a1aae62918c4c9035f9cb7586323515404da6d
jqn <jqngl@icloud.com>
2020-11-24 07:23:29 +0100

    Removed config sql alchemy connection variable

--- config.py
+++ config.py
@@ -26,12 +26,6 @@ class ProductionConfig(Config):
     """
 
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-        username=os.environ['DB_USERNAME'],
-        password=os.environ['DB_PASSWORD'],
-        hostname=os.environ['DB_HOSTNAME'],
-        databasename=os.environ['DB_NAME'],
-    )
 
 
 app_config = {
