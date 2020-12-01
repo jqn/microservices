f933da9554e9ff8ee0d516d7b208d9edf5486dbc
jqn <jqngl@icloud.com>
2020-12-01 06:04:12 +0100

    Removed instance directory with local config

--- instance/config.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'yoursecretkey'
-
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="root",
-    password="root",
-    hostname="localhost",
-    databasename="microservices",
-)
