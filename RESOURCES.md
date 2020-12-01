a47ac9bbedb9b6be460e63678decfcc44ba9fe22
jqn <jqngl@icloud.com>
2020-12-01 05:58:38 +0100

    Added error pages

--- RESOURCES.md
+++ RESOURCES.md
@@ -4,16 +4,17 @@ https://www.codeply.com/go/3l6UhaQEhq/bootstrap-4-chartjs
 https://www.chartjs.org/docs/latest/
 
 [Bootstrap Code Snippets](https://startbootstrap.com/snippets/sign-in-split/)
+
 [Dashboard](https://startbootstrap.com/previews/sb-admin)
 
 ### Resources
 
 [Bootstrap Examples](https://github.com/twbs/bootstrap/tree/2a2b0b5abe7b44b519c9e19e8876b75608979dd0/site/content/docs/4.3/examples)
 
-https://www.digitalocean.com/community/tutorials/build-a-crud-web-app-with-python-and-flask-part-one
+[CRUD app Flask - Part 01](https://www.digitalocean.com/community/tutorials/build-a-crud-web-app-with-python-and-flask-part-one)
 
-https://scotch.io/tutorials/build-a-crud-web-app-with-python-and-flask-part-two
+[CRUD app Flask - Part 02](https://scotch.io/tutorials/build-a-crud-web-app-with-python-and-flask-part-two)
 
-https://scotch.io/tutorials/build-a-crud-web-app-with-python-and-flask-part-three
+[CRUD app Flask - Part 03](https://scotch.io/tutorials/build-a-crud-web-app-with-python-and-flask-part-three)
 
-http://ericplumb.com/blog/passing-apache-environment-variables-to-django-via-mod_wsgi.html
+[Apache env variables](http://ericplumb.com/blog/passing-apache-environment-variables-to-django-via-mod_wsgi.html)
--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # app/__init__.py
 
 # third-party imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -46,19 +46,31 @@ def create_app(config_name):
 
     from app import models
 
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
     from .ratings import ratings as ratings_blueprint
     app.register_blueprint(ratings_blueprint)
 
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-    from .api import api as api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix='/api')
-
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
 
     return app
