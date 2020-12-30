4e8d7d333c2ee5e2928f3d12803abce309bc415a
jqn <jqngl@icloud.com>
2020-12-30 10:22:30 +0100

    remove followers route in api

--- app/api/users.py
+++ app/api/users.py
@@ -10,23 +10,3 @@ def get_user(id):
 @api.route('/users', methods=['POST'])
 def get_users():
     return json.dumps({'status': 'OK', 'user': '', })
-
-
-@api.route('/users/<int:id>/followers', methods=['GET'])
-def get_followers(id):
-    pass
-
-
-@api.route('/users/<int:id>/followed', methods=['GET'])
-def get_followed(id):
-    pass
-
-
-@api.route('/users', methods=['POST'])
-def create_user():
-    pass
-
-
-@api.route('/users/<int:id>', methods=['PUT'])
-def update_user(id):
-    pass
